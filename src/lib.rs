f49ccffffbaf0b154cbb2824f964165227be1172
Martin Hloska <martin.hloska@topmonks.com>
2020-11-16 09:32:06 +0100

    renamed math package

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use hydra_dx_utils::calculate_spot_price;
-use hydra_dx_utils::calculate_sell_price;
-use hydra_dx_utils::calculate_buy_price;
+use hydra_dx_math::calculate_spot_price;
+use hydra_dx_math::calculate_sell_price;
+use hydra_dx_math::calculate_buy_price;
 
 mod wasm;
 
