bea45ab3f83e259852d76cdc20d3953a371ec48a
Martin Hloska <martin.hloska@gmail.com>
2020-12-10 23:35:26 +0100

    hpdated hydra-dx math package name

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use hydra_dx_math::calculate_spot_price;
-use hydra_dx_math::calculate_sell_price;
-use hydra_dx_math::calculate_buy_price;
+use hack_hydra_dx_math::calculate_spot_price;
+use hack_hydra_dx_math::calculate_sell_price;
+use hack_hydra_dx_math::calculate_buy_price;
 
 mod wasm;
 
