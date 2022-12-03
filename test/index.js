d5c195708c1574037c96df49532469a4f5a5223e
mrq <mrq@mrq.cz>
2022-12-03 12:35:32 +0000

    test all packages

--- test/index.js
+++ test/index.js
@@ -1,7 +1,19 @@
-console.log("xyk")
-const xyk = require('@galacticcouncil/math-xyk');
-console.log("bindings", Object.keys(xyk).length);
+console.log("lbp");
+const lbp = require('@galacticcouncil/math-lbp');
+console.log(" bindings", Object.keys(lbp).length);
 
-console.log("omnipool")
+console.log("liquidity-mining");
+const lm = require('@galacticcouncil/math-liquidity-mining');
+console.log(" bindings", Object.keys(lm).length);
+
+console.log("stableswap");
+const stableswap = require('@galacticcouncil/math-stableswap');
+console.log(" bindings", Object.keys(stableswap).length);
+
+console.log("xyk");
+const xyk = require('@galacticcouncil/math-xyk');
+console.log(" bindings", Object.keys(xyk).length);
+
+console.log("omnipool");
 const omnipool = require('@galacticcouncil/math-omnipool');
-console.log("bindings", Object.keys(omnipool).length);
+console.log(" bindings", Object.keys(omnipool).length);
