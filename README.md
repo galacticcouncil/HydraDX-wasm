8c681e0ac78efcccb696ad54a5ce16c62a4b59dd
mrq <mrq@mrq.cz>
2022-12-03 12:19:27 +0000

    test bindings

--- README.md
+++ README.md
@@ -14,3 +14,13 @@
 1. `make build-package`
 2. `npm publish --workspaces --access public`
 3. `make clean-package`
+
+## Test 
+
+After packages are built by `make build-package`
+
+```
+cd test
+npm install
+npm run test
+```
---
+++ test/index.js
@@ -0,0 +1,7 @@
+console.log("xyk")
+const xyk = require('@galacticcouncil/math-xyk');
+console.log("bindings", Object.keys(xyk).length);
+
+console.log("omnipool")
+const omnipool = require('@galacticcouncil/math-omnipool');
+console.log("bindings", Object.keys(omnipool).length);
