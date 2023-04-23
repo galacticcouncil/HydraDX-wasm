f56070548706a8bc8985a2e713a21edc0522460a
Pavol Noha <pavol.noha@gmail.com>
2023-04-23 22:52:24 +0200

    npm v0.1.2

--- build-package.sh
+++ build-package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cp build/$1/bundler/hydra_dx_wasm_bg.js packages/$1/index.esm.js
+cp build/$1/bundler/hydra_dx_wasm.js packages/$1/index.esm.js
 cp build/$1/bundler/hydra_dx_wasm.d.ts packages/$1/index.d.ts
+cp build/$1/bundler/hydra_dx_wasm_bg.js packages/$1/hydra_dx_wasm_bg.js
 cp build/$1/bundler/hydra_dx_wasm_bg.wasm packages/$1/hydra_dx_wasm_bg.wasm
 cp build/$1/bundler/hydra_dx_wasm_bg.wasm.d.ts packages/$1/hydra_dx_wasm_bg.wasm.d.ts
 cp build/$1/nodejs/hydra_dx_wasm.js packages/$1/index.js
