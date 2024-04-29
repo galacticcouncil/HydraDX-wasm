03bcfb1027bae75b66da3f048b9319e388ff329e
dmoka <mokabme@gmail.com>
2024-04-29 16:59:55 +0200

    LBP: add spot price calculation with and without fee

--- src/lib.rs
+++ src/lib.rs
@@ -251,6 +251,8 @@ pub mod xyk {
 
 #[cfg(feature = "lbp")]
 pub mod lbp {
+    use num_traits::Zero;
+    use sp_arithmetic::FixedU128;
     pub use super::*;
 
     #[wasm_bindgen]
@@ -263,6 +265,30 @@ pub mod lbp {
 
         result.unwrap_or(0).to_string()
     }
+
+    #[wasm_bindgen]
+    pub fn calculate_spot_price(s: String, b: String, s_w: String, b_w: String) -> String {
+        let (sell_reserve, buy_reserve) = to_u128!(s, b);
+        let (sell_weight, buy_weight) = to_u32!(s_w, b_w);
+
+        let result =
+            hydra_dx_math::lbp::spot_price(sell_reserve, buy_reserve, sell_weight, buy_weight, 0, 0, None);
+
+        result.unwrap_or(FixedU128::zero()).to_string()
+    }
+
+
+    #[wasm_bindgen]
+    pub fn calculate_spot_price_with_fee(s: String, b: String, s_w: String, b_w: String, fee_asset: String, asset_out: String, fee_rate_n: String, fee_rate_d: String ) -> String {
+        let (sell_reserve, buy_reserve) = to_u128!(s, b);
+        let (sell_weight, buy_weight, fee_asset, asset_out, fee_rate_n, fee_rate_d) = to_u32!(s_w, b_w, fee_asset, asset_out, fee_rate_n, fee_rate_d);
+
+        let result =
+            hydra_dx_math::lbp::spot_price(sell_reserve, buy_reserve, sell_weight, buy_weight, fee_asset, asset_out, Some((fee_rate_n, fee_rate_d)));
+
+        result.unwrap_or(FixedU128::zero()).to_string()
+    }
+
 
     #[wasm_bindgen]
     pub fn calculate_out_given_in(s: String, b: String, s_w: String, b_w: String, a: String) -> String {
@@ -323,6 +349,75 @@ pub mod lbp {
             ),
             "0"
         );
+    }
+
+    #[test]
+    fn calculate_spot_price_works() {
+        assert_eq!(
+            lbp::calculate_spot_price(
+                String::from("1000"),
+                String::from("2000"),
+                String::from("1000"),
+                String::from("2000"),
+            ),
+            "1000000000000000000"
+        );
+
+        assert_eq!(
+            lbp::calculate_spot_price(
+                String::from("1000"),
+                String::from("2000"),
+                String::from("0"),
+                String::from("2000"),
+            ),
+            "0"
+        );
+    }
+
+    #[test]
+    fn calculate_spot_price_with_fee_works() {
+        assert_eq!(
+            lbp::calculate_spot_price_with_fee(
+                String::from("1000"),
+                String::from("2000"),
+                String::from("1000"),
+                String::from("2000"),
+                String::from("5"),
+                String::from("5"),
+                String::from("1"),
+                String::from("100")
+            ),
+            "990000000000000000"
+        );
+
+        assert_eq!(
+            lbp::calculate_spot_price_with_fee(
+                String::from("1000"),
+                String::from("2000"),
+                String::from("1000"),
+                String::from("2000"),
+                String::from("1"),
+                String::from("5"),
+                String::from("1"),
+                String::from("100")
+            ),
+            "1000000000000000000"
+        );
+
+        assert_eq!(
+            lbp::calculate_spot_price_with_fee(
+                String::from("1000"),
+                String::from("2000"),
+                String::from("1000"),
+                String::from("0"),
+                String::from("1"),
+                String::from("5"),
+                String::from("1"),
+                String::from("100")
+            ),
+            "0"
+        );
+
     }
 
     #[test]
